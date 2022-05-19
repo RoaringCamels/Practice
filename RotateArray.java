0c55f67bbbd9b2f97ff0cf9871d1ef17480fbb14
Ivan <revillaivan97@gmail.com>
2022-05-19 10:34:09 -0700

    Finished

--- RotateArray.java
+++ RotateArray.java
@@ -9,7 +9,7 @@ public class RotateArray {
 
         System.out.println();
 
-        rotateLeft(nums, 2);
+        rotateLeft(nums, 2, nums.length);
         System.out.println("The array after rotating to the left n times: ");
         printStandard(nums);
     }
@@ -20,14 +20,20 @@ public class RotateArray {
      * @param array          An array with type int
      * @param amountToRotate The amount of rotations we want of typ int
      */
-    public static void rotateLeft(int array[], int amountToRotate) {
+    public static void rotateLeftByOne(int array[], int n) {
         int temp;
         int i;// pointer to keep track of where we are in the array
         temp = array[0]; // temporary array to hold vlues
-        for (i = 0; i < array.length; i++) { // traverse through the array
+        for (i = 0; i < n - 1; i++) { // traverse through the array
             array[i] = array[i + 1];// shifts the values of the array to the left
         }
         array[i] = temp;// set the counter to the temporary array
+    }
+
+    public static void rotateLeft(int array[], int d, int n) {
+        for (int i = 0; i < d; i++) {
+            rotateLeftByOne(array, n);
+        }
     }
 
     /**
